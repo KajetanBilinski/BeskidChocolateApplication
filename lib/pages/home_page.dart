3008abe1667437351f0c3ada06d73a6560e2b61f
KajetanBilinski <75189414+KajetanBilinski@users.noreply.github.com>
2023-09-12 23:53:52 +0200

    Updated HomePage
    Responsivness added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,21 +3,23 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'about_us_page.dart';
+import '../utils/GlobalVariables.dart';
 
 class HomePage extends StatefulWidget {
+
+  //GlobalVariables. = MediaQuery.of(context).size.width * 0.05;
+
   const HomePage({super.key});
 
   @override
   _HomePageState createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
   final double fontsize = 23.0;
   final PageController _pageController = PageController();
   final List<String> imagePaths = [
     'assets/images/home_page/background_1.jpg',
     'assets/images/home_page/background_2.png'
-    // Add more image paths here
   ];
   int currentPage = 0;
 
@@ -51,9 +53,37 @@ class _HomePageState extends State<HomePage> {
     _pageController.dispose();
     super.dispose();
   }
+  void initializeGlobalValues()
+  {
+    GlobalVariables.fontSizeButtonHomePage = MediaQuery.of(context).size.height * 0.06;
+    GlobalVariables.buttonHeightHomePage = MediaQuery.of(context).size.height * 0.067;
+    GlobalVariables.buttonWidthHomePage = MediaQuery.of(context).size.width * 0.55;
+    GlobalVariables.spaceButtonsHomePage = MediaQuery.of(context).size.height * 0.06;
+    GlobalVariables.spaceIconHomePage = MediaQuery.of(context).size.height * 0.1;
 
+    GlobalVariables.fontSizeTitleAboutUs  = MediaQuery.of(context).size.height* 0.027;
+    GlobalVariables.fontSizeTextAboutUs  = MediaQuery.of(context).size.height * 0.022;
+    GlobalVariables.fontSizeButtonAboutUs  = MediaQuery.of(context).size.height * 0.021;
+    GlobalVariables.buttonWidthAboutUs  = MediaQuery.of(context).size.width * 0.4;
+    GlobalVariables.buttonHeightAboutUs  = MediaQuery.of(context).size.height * 0.045;
+    GlobalVariables.iconSizeAboutUs  = MediaQuery.of(context).size.width * 0.06;
+    GlobalVariables.noteWidthAboutUs  = MediaQuery.of(context).size.width * 0.95;
+    GlobalVariables.noteHeightAboutUs  = MediaQuery.of(context).size.height * 0.35;
+    GlobalVariables.spaceTitleTextAboutUs  = MediaQuery.of(context).size.height * 0.02;
+    GlobalVariables.spaceTextAboutUs  = MediaQuery.of(context).size.height * 0.02;
+    GlobalVariables.spaceIconTextAboutUs  = MediaQuery.of(context).size.width * 0.04;
+    GlobalVariables.spaceNoteHeightAboutUs  = MediaQuery.of(context).size.height * 0.1;
+    GlobalVariables.spaceNoteWidthAboutUs  = MediaQuery.of(context).size.width * 0.07;
+    GlobalVariables.spaceRowNoteAboutUs  = MediaQuery.of(context).size.height * 0.1;
+    GlobalVariables.spaceButtonAboutUs  = MediaQuery.of(context).size.height * 0.025;
+    GlobalVariables.spaceText2AboutUs  = MediaQuery.of(context).size.height * 0.025;
+
+
+
+  }
   @override
   Widget build(BuildContext context) {
+    initializeGlobalValues();
     return MaterialApp(
       home: Scaffold(
         body: Stack(
@@ -80,12 +110,12 @@ class _HomePageState extends State<HomePage> {
               AspectRatio(
                 aspectRatio: 3/1, // Use the calculated aspect ratio
                 child: Image.asset(
-                  'assets/icons/logo.png' // Replace with your image path
+                  'assets/icons/home_page/logo.png' // Replace with your image path
                    // Adjust the fit property as needed
                 ),
               ),
 
-              SizedBox(height: 50.0), // Odstęp między logo a przyciskami
+              SizedBox(height: GlobalVariables.spaceIconHomePage), // Odstęp między logo a przyciskami
 
               ElevatedButton(
                 onPressed: () {
@@ -100,22 +130,21 @@ class _HomePageState extends State<HomePage> {
                   foregroundColor: Colors.white,
                   side: BorderSide(color: Colors.white, width: 2.0),
                   shape: RoundedRectangleBorder(
-
                     borderRadius: BorderRadius.circular(20.0),
                   ),
-                  minimumSize: Size(210.0, 55.0),
+                  minimumSize: Size(GlobalVariables.buttonWidthHomePage, GlobalVariables.buttonHeightHomePage),
                 ),
                 child: Text(
                   'O nas',
                   style: TextStyle(
-                      fontSize: fontsize,
+                      fontSize: GlobalVariables.fontSizeButtonHomePage,
                       color: Colors.white,
                       fontFamily: 'Lato'
                   ),
 
                 ),
               ),
-              SizedBox(height: 15.0), // Odstęp między przyciskami
+              SizedBox(height: GlobalVariables.spaceButtonsHomePage),// Odstęp między przyciskami
               ElevatedButton(
                 onPressed: () {
 
@@ -127,17 +156,17 @@ class _HomePageState extends State<HomePage> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20.0),
                   ),
-                  minimumSize: Size(210.0, 55.0),
+                  minimumSize: Size(GlobalVariables.buttonWidthHomePage, GlobalVariables.buttonHeightHomePage),
                 ),
                 child: Text(
                   'Informacje',
                   style: TextStyle(
-                      fontSize: fontsize,
+                      fontSize: GlobalVariables.fontSizeButtonHomePage,
                       color: Colors.white,
                       fontFamily: 'Lato'
                   ),),
               ),
-              SizedBox(height: 15.0),
+              SizedBox(height: GlobalVariables.spaceButtonsHomePage),
               ElevatedButton(
                 onPressed: () {
 
@@ -149,18 +178,18 @@ class _HomePageState extends State<HomePage> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20.0),
                   ),
-                  minimumSize: Size(210.0, 55.0),
+                  minimumSize: Size(GlobalVariables.buttonWidthHomePage, GlobalVariables.buttonHeightHomePage),
                 ),
                 child: Text(
                   'Zwiedzanie',
                   style: TextStyle(
-                      fontSize: fontsize,
+                      fontSize: GlobalVariables.fontSizeButtonHomePage,
                       color: Colors.white,
                       fontFamily: 'Lato'
                   ),
                 ),
               ),
-              SizedBox(height: 15.0),
+              SizedBox(height: GlobalVariables.spaceButtonsHomePage),
               ElevatedButton(
                 onPressed: () {
                   _launchWebsite();
@@ -172,12 +201,12 @@ class _HomePageState extends State<HomePage> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20.0),
                   ),
-                  minimumSize: Size(210.0, 55.0),
+                  minimumSize: Size(GlobalVariables.buttonWidthHomePage, GlobalVariables.buttonHeightHomePage),
                 ),
                 child: Text(
                   'Oceń aplikacje',
                   style: TextStyle(
-                      fontSize: fontsize,
+                      fontSize: GlobalVariables.fontSizeButtonHomePage,
                       color: Colors.white,
                       fontFamily: 'Lato'
                   ),

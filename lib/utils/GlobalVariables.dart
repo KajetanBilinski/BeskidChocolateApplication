e25b9e06bfd3c8e1dbd379952ff811a3bf870924
KajetanBilinski <75189414+KajetanBilinski@users.noreply.github.com>
2023-09-12 21:35:51 +0200

    GlobalVariablesClass
    Class of variables for responsiveness.
    HomePage done

---
+++ lib/utils/GlobalVariables.dart
@@ -0,0 +1,19 @@
+
+class GlobalVariables {
+  static double fontSizeButtonHomePage = 0.0;
+  static double buttonWidthHomePage = 0.0;
+  static double buttonHeightHomePage = 0.0;
+  static double spaceButtonsHomePage=0.0;
+  static double spaceIconHomePage=0.0;
+
+  static double fontSizeText = 0.0;
+  static double fontSizeTitle = 0.0;
+
+
+
+
+  static double buttonWidth = 0.0;
+  static double buttonHeight = 0.0;
+
+
+}

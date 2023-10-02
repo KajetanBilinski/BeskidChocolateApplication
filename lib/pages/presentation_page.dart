616dfe82c4b0f20c5183ac744020fd2ec17f79ff
KajetanBilinski <75189414+KajetanBilinski@users.noreply.github.com>
2023-10-02 23:46:57 +0200

    presentation page update

--- lib/pages/presentation_page.dart
+++ lib/pages/presentation_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import '../models/Field.dart';
 import '../utils/GlobalComponents.dart';
 import '../utils/GlobalVariables.dart';
-import 'home_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class PresentationPage extends StatelessWidget {
@@ -16,59 +15,122 @@ class PresentationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            body: Center(
-                child:
-                ListView(
-                    padding: EdgeInsets.zero,
-                    children: [
-                      GlobalComponents.backArrow(context,PresentationMain(),true,false),
-                      const SizedBox(height: 25),
-                      for(Field? f in UtilsJson.PPPageFields![number]!)
-                        Column(
-                          children: [
-                            if(f!.Type == 'text')
-                              Text(
-                                f.Content,
-                                style: const TextStyle(
-                                    fontFamily: 'Lato',
-                                    fontSize: 10
-                                ),
-                              )
-                            else if(f.Type == 'image')
-                              Image.asset(
-                                  f.Content,
-                                  fit: BoxFit.cover
-                              )
-                            else if(f.Type == 'title')
-                                Text(
-                                    f.Content,
-                                    style: const TextStyle(
+            body:
+                Stack(
+                  children: [
+                   ListView(
+                        padding: EdgeInsets.zero,
+                        children: [
+                          GlobalComponents.backArrow(context,PresentationMain(),true,false),
+                          const SizedBox(height: 25),
+                          for(Field? f in UtilsJson.PPPageFields![number]!)
+                            Column(
+                              children: [
+                                if(f!.Type == 'text')
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                    child: Text(
+                                      f.Content,
+                                      style: const TextStyle(
                                         fontFamily: 'Lato',
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold
-                                    )
-                                )
-                              else if(f.Type == 'video')
-                                  InkWell(
-                                    onTap: () {
-                                      _launchWebsite(f.Link.toString());
-                                    },
-                                    child:
-                                    Image.asset(
-                                        f.Content,
-                                        fit: BoxFit.cover
+                                        fontSize: 13,
+                                      ),
                                     ),
-                                  ),
-                            const SizedBox(height: 10)
+                                  )
+                                else if(f.Type == 'image')
+                                  Image.asset(
+                                      f.Content,
+                                      fit: BoxFit.cover
+                                  )
+                                else if(f.Type == 'title')
+                                    Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                                        child: Text(
+                                            f.Content,
+                                            style: const TextStyle(
+                                                fontFamily: 'Lato',
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold
+                                            )
+                                        )
+                                    )
+
+                                  else if(f.Type == 'video')
+                                      InkWell(
+                                        onTap: () {
+                                          _launchWebsite(f.Link.toString());
+                                        },
+                                        child:
+                                        Image.asset(
+                                            f.Content,
+                                            fit: BoxFit.cover
+                                        ),
+                                      ),
+                                const SizedBox(height: 10)
+                              ],
+
+                            ),
+                          const SizedBox(height: 40)
+
+
+                        ]
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        color: Colors.white,
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            if(number!=0)
+                            InkWell(
+                              onTap: () {
+                                int nextPage = number;
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => PresentationPage(number: --nextPage))
+                                );
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                                child: Image.asset(
+                                  "assets/icons/presentation_page/leftArrow.png",
+                                ),
+                              ),
+                            )
+                            else
+                              SizedBox(width: 30),
+                            if(number!=GlobalVariables.maxPages-1)
+                            InkWell(
+                              onTap: () {
+                                int nextPage = number;
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => PresentationPage(number: ++nextPage))
+                                );
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                                child: Image.asset(
+                                  "assets/icons/presentation_page/rightArrow.png",
+                                ),
+                              ),
+                            )
+                            else
+                              SizedBox(width: 30),
+
                           ],
-
                         )
-
-
-                    ]
+                        ),
+                      ),
+                  ],
                 )
-            )
+
         )
+
     );
 
   }
